3468d72f2cfa19356de693ffbf605d371af2cb48
Mayco Cortez <maycolp1@gmail.com>
2022-07-10 15:03:26 -0300

    Subida del proyecto

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//obtenemos el divRoot
+const divRoot = document.getElementById('root');
+//creamos el virtual DOM
+const root = ReactDOM.createRoot(divRoot)
+//dibujar los componentes de React aca dentro
 root.render(
   <React.StrictMode>
     <App />
