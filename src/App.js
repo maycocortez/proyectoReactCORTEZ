e645a78b86d3df6d1f4e493dee21d5c4019da7f4
Mayco Cortez <maycolp1@gmail.com>
2022-07-14 21:53:04 -0300

    Agregado navBar sencillo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import './components/NavBar.css'
 
 function App() {
 
-  const name = "Gonzalo"
+  
   return (
-    <div className="App">
+   <>
+      <NavBar/>
   
         
-        <h1>
-          Hola {name}!
-        </h1>
-   
-    </div>
-  );
+      </>
+  )
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+
+
+const NavBar = () => {
+
+return(
+<div className="divNav">
+<nav>
+    <ul>
+        <li>DISQUERIA CORTEZ</li>
+        <li><a href="#">CATALOGO</a></li>
+        <li><a href="#">NOSOTROS</a></li>
+        <li><a href="#">CONTACTO</a></li>
+    </ul>
+</nav>
+</div> 
+)
+}
+
+
+export default NavBar
+
